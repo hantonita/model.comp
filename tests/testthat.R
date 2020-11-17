aff1802e769dbe08f12875bf52106035097e63ff
Hanna Frank <hanna.a.frank@gmail.com>
2020-11-16 22:05:37 -0800

    Add tests

---
+++ tests/testthat.R
@@ -0,0 +1,4 @@
+library(testthat)
+library(model.comp)
+
+test_check("model.comp")
---
+++ tests/testthat/test-compare.R
@@ -0,0 +1,35 @@
+library(gapminder)
+test_data <- gapminder::gapminder
+
+test_that("compare function works for logistic regression model", {
+  # create the expected dataframe containg model & fitted values
+  mod <- glm(data = gapminder, formula = lifeExp ~gdpPercap)
+  fitted_vals <- list(list(fitted(mod)))
+  df <- data.frame(matrix(ncol = 2, nrow = 1), row.names = c('logreg'))
+  colnames(df) <- c('fitted_model', 'fitted_values')
+  df['logreg', 'fitted_model'] <- list(mod)
+  df['logreg', 'fitted_values'] <- fitted_vals
+
+  # run test
+  expect_equal(compare(data = test_data, eqn = "lifeExp ~ gdpPercap"), df)
+})
+
+test_that("compare function works for multiple models", {
+  mod <- glm(data = gapminder, formula = lifeExp ~gdpPercap)
+  fitted_vals <- list(list(fitted(mod)))
+  df <- data.frame(matrix(ncol = 2, nrow = 1), row.names = c('logreg'))
+  colnames(df) <- c('fitted_model', 'fitted_values')
+  df['logreg', 'fitted_model'] <- list(mod)
+  df['logreg', 'fitted_values'] <- fitted_vals
+  expect_equal(compare(data = test_data, eqn = "lifeExp ~ gdpPercap"), df)
+})
+
+test_that("compare function works for logistic regression model", {
+  mod <- glm(data = gapminder, formula = lifeExp ~gdpPercap)
+  fitted_vals <- list(list(fitted(mod)))
+  df <- data.frame(matrix(ncol = 2, nrow = 1), row.names = c('logreg'))
+  colnames(df) <- c('fitted_model', 'fitted_values')
+  df['logreg', 'fitted_model'] <- list(mod)
+  df['logreg', 'fitted_values'] <- fitted_vals
+  expect_equal(compare(data = test_data, eqn = "lifeExp ~ gdpPercap"), df)
+})
