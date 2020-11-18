13ee0b538fb5d82b39c6f7108a3fb2e68e2ab8ca
Hanna Frank <hanna.a.frank@gmail.com>
2020-11-18 11:19:13 -0800

    Update documentation

---
+++ R/model.comp.R
@@ -0,0 +1,11 @@
+#' model.comp: a package for model comparisons
+#'
+#' The model.comp package currently provides the compare_models() function which
+#' fits specified models and evaluates specified performance metrics for a given
+#' dataset.
+#'
+#'
+#' @docType package
+#' @name model.comp
+NULL
+#> NULL
